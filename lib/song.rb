04dd083f1def4862cbe0567ca90ffe0204e74348
Bryecook <brybcook@gmail.com>
2021-02-25 07:53:23 -0800

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,66 @@
+require 'pry'
+
+class Song
+
+    @@count=0
+    @@artists=[]
+    @@genres=[]
+
+    def initialize(name, artist, genre)
+        @name=name
+        @artist=artist
+        @genre=genre
+        @@count+=1
+        @@artists << @artist
+        @@genres << @genre
+    end
+
+    def name
+        @name
+    end
+
+    def artist
+        @artist
+    end
+    
+    def genre
+        @genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        hash={}
+        @@genres.each do |genre|
+            if hash[genre]
+                hash[genre]+=1
+            else
+                #binding.pry
+                hash[genre]= 1
+            end
+        end
+        hash
+    end
+
+    def self.artist_count
+        artists={}
+        @@artists.each do |artist|
+            if artists[artist]
+                artists[artist]+=1
+            else
+                artists[artist]=1
+            end
+        end
+        artists
+    end
+end
